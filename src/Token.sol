abd00249c366b8f83169334fb71d16e62be2e224
Monster <bf.monster@proton.me>
2024-10-02 15:47:44 +0200

    lint

--- src/Token.sol
+++ src/Token.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.25;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    uint256 public constant initialSupply = 777_777_777_777 ether;
-
     constructor(address receiver) ERC20("BET", "BET") {
-        _mint(receiver, initialSupply);
+        _mint(receiver, 777_777_777_777 ether);
     }
 }
